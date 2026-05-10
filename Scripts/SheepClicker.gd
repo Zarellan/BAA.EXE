0b95eff47668e168ce05c82150d30fbdf26c74d0
Zarellan <khalilyousef796@gmail.com>
2026-05-10 20:19:29 +0400

    update 1

---
+++ Scripts/SheepClicker.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+var isInside = false
+
+var twe:Tween
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (isInside && Input.is_action_just_pressed("LeftMouse")):
+		print("PRESSED YAY")
+	pass
+
+
+func _on_static_body_2d_mouse_entered() -> void:
+	isInside = true
+	pass # Replace with function body.
+
+
+func _on_static_body_2d_mouse_exited() -> void:
+	isInside = false
+	pass # Replace with function body.
---
+++ Scripts/Utils/TweenUtils.gd
@@ -0,0 +1,35 @@
+class_name TweenUtils
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+static func tweenX(object,position,duration,trans,ease = Tween.EASE_IN_OUT):
+	var tween = object.create_tween()
+	tween.tween_property(object, "position:x", position, duration).set_trans(trans).set_ease(ease)
+	return tween
+
+static func tweenY(object,position,duration,trans,ease = Tween.EASE_IN_OUT):
+	var tween = object.create_tween()
+	tween.tween_property(object, "position:y", position, duration).set_trans(trans).set_ease(ease)
+	return tween
+
+static func tweenScaleX(object,position,duration,trans,ease = Tween.EASE_IN_OUT):
+	var tween = object.create_tween()
+	tween.tween_property(object, "scale:x", position, duration)\
+	.set_trans(trans)\
+	.set_ease(ease)
+	return tween
+
+static func tweenScaleY(object,position,duration,trans,ease = Tween.EASE_IN_OUT):
+	var tween = object.create_tween()
+	tween.tween_property(object, "scale:y", position, duration)\
+	.set_trans(trans)\
+	.set_ease(ease)
+	return tween
