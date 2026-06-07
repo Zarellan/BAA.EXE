bdcabc43e710900919bc489aaacbab256ffabb0f
Zarellan <Khalilyousef796@gmail.com>
2026-06-07 22:21:15 +0400

    material duplicate

--- Scripts/ParticlesGolds/RareWoolGoldShade.gd
+++ Scripts/ParticlesGolds/RareWoolGoldShade.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
-
+var parent:Node2D
+var dupMaterial:ShaderMaterial
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	goldWoolValue = ValueSaver.new()
+	parent = get_parent()
 	GoldWool()
+	dupMaterial = parent.material.duplicate()
+	parent.material = dupMaterial
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	get_parent().material.set_shader_parameter("progress", goldWoolValue.number)
+	dupMaterial.set_shader_parameter("progress", goldWoolValue.number)
 	pass
 
 var goldWoolTween:Tween
