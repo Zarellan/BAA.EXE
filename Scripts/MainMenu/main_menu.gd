0392eba967e42f5b8405ac5bd68c2472e4c0a017
Zarellan <Khalilyousef796@gmail.com>
2026-06-19 20:19:37 +0400

    skins is making

--- Scripts/MainMenu/main_menu.gd
+++ Scripts/MainMenu/main_menu.gd
@@ -3,12 +3,20 @@ extends Control
 @export var bar:TextureProgressBar
 @export var scenePath: String
 @export var background: TextureRect
+@export var sheep: Sprite2D
+@export var tex: Control
 
 var progress : Array[float] = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#ResourceLoader.load_threaded_request(scenePath)
+	TweenUtils.tweenScalePingPong(background,Vector2(1,1.05),Vector2(1,1),2,TweenUtils.Ease.InOutSine)
+	TweenUtils.tweenSkewPingPong(sheep,-0.04,0.04,1,TweenUtils.Ease.InOutSine)
+	await get_tree().create_timer(1).timeout
+	TweenUtils.tweenY(tex,-62.0,0.3,TweenUtils.Ease.OutCirc)
+	await get_tree().create_timer(0.5).timeout
 	ResourceLoader.load_threaded_request(scenePath)
-	TweenUtils.tweenScalePingPong(background,Vector2(1,1),Vector2(1,1.05),2,TweenUtils.Ease.InOutSine)
+	bar.visible = true
 	pass # Replace with function body.
 
 
---
+++ Scripts/SkinsResources/SkinItem.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name SkinItem
+
+@export var name = "def"
+@export var value = 2
